723e69fa7a173d762778de97008146e895c05b77
MoonsuKang <rkdms4971@naver.com>
2024-04-05 03:03:47 +0900

    [ADD/#3] Add NavigationKeys for centralized navigation path

---
+++ app/src/main/java/com/sopt/now/compose/navigation/NavigationKeys.kt
@@ -0,0 +1,14 @@
+package com.sopt.now.compose.navigation
+
+object NavRoutes {
+    const val SignUp = "signup"
+    const val Login = "login"
+    const val Main = "main"
+}
+
+object NavArgs {
+    const val UserId = "userId"
+    const val UserPwd = "userPwd"
+    const val Nickname = "nickname"
+    const val Mbti = "mbti"
+}
