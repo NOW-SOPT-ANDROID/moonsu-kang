929840806f23dfc1f2b50a92ee6d0af75b146c45
MoonsuKang <rkdms4971@naver.com>
2024-04-08 20:09:31 +0900

    [REFACTOR/#1] ToastMessage Resource ID and add ?

--- app/src/main/java/com/sopt/now/utils/ActivityUtils.kt
+++ app/src/main/java/com/sopt/now/utils/ActivityUtils.kt
@@ -4,12 +4,13 @@ import android.app.Activity
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
 // 토스트 메시지
-fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, message, duration).show()
+fun Activity.toast(@StringRes messageResId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, getString(messageResId), duration).show()
 }
 
 // 스낵바 메시지
@@ -20,8 +21,8 @@ fun Activity.snackBar(
 ) {
     val snackbar = Snackbar.make(view, message, duration)
     val snackbarView = snackbar.view
-    val textView = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView // Snackbar 텍스트 색상을 변경하려면, TextView 찾아야됨.
-    textView.setTextColor(ContextCompat.getColor(this, android.R.color.black)) // 텍스트 색상 변경.
+    val textView = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as? TextView // Snackbar 텍스트 색상을 변경하려면, TextView 찾아야됨.
+    textView?.setTextColor(ContextCompat.getColor(this, android.R.color.black)) // 텍스트 색상 변경.
 
     snackbar.show()
 }
