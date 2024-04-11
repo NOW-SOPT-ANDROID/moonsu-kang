5086b80819c6989dd6993e2e21d9f86cd0481589
MoonsuKang <rkdms4971@naver.com>
2024-04-11 19:51:36 +0900

    [REFACTOR/#5] Nullable로 수정 및 클래스명 수정

--- app/src/main/java/com/sopt/now/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/now/presentation/mypage/MyPageViewModel.kt
@@ -1,16 +1,16 @@
-package com.sopt.now.presentation.main
+package com.sopt.now.presentation.mypage
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sopt.now.model.User
 
-class MainViewModel : ViewModel() {
+class MyPageViewModel : ViewModel() {
     // 로그인 정보를 담은 User를 LiveData로 관리
-    private val _userInfo = MutableLiveData<User>()
-    val userInfo: LiveData<User> get() = _userInfo
+    private val _userInfo = MutableLiveData<User?>()
+    val userInfo: LiveData<User?> get() = _userInfo
 
-    fun setUserInfo(user: User) {
+    fun setUserInfo(user: User?) {
         _userInfo.value = user
     }
 }
