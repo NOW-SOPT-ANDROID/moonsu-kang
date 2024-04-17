b1b9a62d6fd63a8b71be268df9a7210e2d77b0db
Moonsu Kang <rkdms4971@naver.com>
2024-04-17 13:18:50 +0900

    [UI/#7] SearchScreen 추가

---
+++ app/src/main/java/com/sopt/now/compose/screen/SearchScreen.kt
@@ -0,0 +1,13 @@
+package com.sopt.now.compose.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+    Column {
+        Text(text = "서치")
+    }
+}
