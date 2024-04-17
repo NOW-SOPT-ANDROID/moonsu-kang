263d7de62b7cb48ffdc6a8c315fee5ff5a5d216e
Moonsu Kang <rkdms4971@naver.com>
2024-04-17 13:05:37 +0900

    [FEAT/#7] BottomNavigation 구현

--- app/src/main/java/com/sopt/now/compose/navigation/AppNavigation.kt
+++ app/src/main/java/com/sopt/now/compose/navigation/AppNavigation.kt
@@ -5,7 +5,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sopt.now.compose.screen.LoginScreen
-import com.sopt.now.compose.screen.MainScreen
 import com.sopt.now.compose.screen.SignUpScreen
 
 @Composable
@@ -38,28 +37,13 @@ fun AppNavigation() {
             LoginScreen(navController, userId, userPwd, nickname, mbti)
         }
 
-        // MainScreen으로 이동하는 경로
         composable(
-            route = "${NavRoutes.MAIN}/{${NavArgs.USER_ID}}/{${NavArgs.USER_PWD}}/{${NavArgs.NICKNAME}}/{${NavArgs.MBTI}}",
-
+            route = "${NavRoutes.HOME}/{${NavArgs.USER_ID}}/{${NavArgs.NICKNAME}}/{${NavArgs.MBTI}}",
         ) { backStackEntry ->
             val userId = backStackEntry.arguments?.getString(NavArgs.USER_ID) ?: ""
-            val userPwd = backStackEntry.arguments?.getString(NavArgs.USER_PWD) ?: ""
             val nickname = backStackEntry.arguments?.getString(NavArgs.NICKNAME) ?: ""
             val mbti = backStackEntry.arguments?.getString(NavArgs.MBTI) ?: ""
-
-            // 추출한 사용자 정보를 MainScreen에 전달
-            MainScreen(userId, userPwd, nickname, mbti)
+            BottomNavigation(userId, nickname, mbti)
         }
-        /**
-         * 명시적으로 인수의 타입을 지정하는 것을 추가해도 된다.
-         * 이걸 추가하면 +타입 안정성, +Type Miss Match 오류 방지
-        arguments = listOf(
-        navArgument("userId") { type = NavType.StringType },
-        navArgument("userPwd") { type = NavType.StringType },
-        navArgument("nickname") { type = NavType.StringType },
-        navArgument("mbti") { type = NavType.StringType }
-        )
-         */
     }
 }
