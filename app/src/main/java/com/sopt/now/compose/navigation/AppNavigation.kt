1e8fed5b8b069a797088f5933f0660e5254446cb
MoonsuKang <rkdms4971@naver.com>
2024-05-03 21:16:44 +0900

    [MOD/#11] 네비게이션 그래프 단순화

--- app/src/main/java/com/sopt/now/compose/navigation/AppNavigation.kt
+++ app/src/main/java/com/sopt/now/compose/navigation/AppNavigation.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.sopt.now.compose.screen.LoginScreen
-import com.sopt.now.compose.screen.SignUpScreen
+import com.sopt.now.compose.screen.auth.login.LoginScreen
+import com.sopt.now.compose.screen.auth.signup.SignUpScreen
 
 @Composable
 fun AppNavigation() {
@@ -16,34 +16,15 @@ fun AppNavigation() {
     NavHost(navController = navController, startDestination = NavRoutes.LOGIN) {
         // Go SignUpScreen
         composable(NavRoutes.SIGN_UP) {
-            SignUpScreen(navController)
+            SignUpScreen(navController = navController)
         }
 
-        // 회원가입 후 넘어온 사용자 정보를 받기 위한 인자값 없는 로그인 화면
         composable(NavRoutes.LOGIN) {
-            LoginScreen(navController)
+            LoginScreen(navController = navController)
         }
 
-        // LoginScreen으로 넘어온 사용자 정보를 기반으로 로그인을 시도하는 경로
-        composable(
-            route = "${NavRoutes.LOGIN}/{${NavArgs.USER_ID}}/{${NavArgs.USER_PWD}}/{${NavArgs.NICKNAME}}/{${NavArgs.MBTI}}",
-        ) { backStackEntry ->
-            val userId = backStackEntry.arguments?.getString(NavArgs.USER_ID) ?: ""
-            val userPwd = backStackEntry.arguments?.getString(NavArgs.USER_PWD) ?: ""
-            val nickname = backStackEntry.arguments?.getString(NavArgs.NICKNAME) ?: ""
-            val mbti = backStackEntry.arguments?.getString(NavArgs.MBTI) ?: ""
-
-            // 추출한 사용자 정보를 LoginScreen에 전달
-            LoginScreen(navController, userId, userPwd, nickname, mbti)
-        }
-
-        composable(
-            route = "${NavRoutes.HOME}/{${NavArgs.USER_ID}}/{${NavArgs.NICKNAME}}/{${NavArgs.MBTI}}",
-        ) { backStackEntry ->
-            val userId = backStackEntry.arguments?.getString(NavArgs.USER_ID) ?: ""
-            val nickname = backStackEntry.arguments?.getString(NavArgs.NICKNAME) ?: ""
-            val mbti = backStackEntry.arguments?.getString(NavArgs.MBTI) ?: ""
-            BottomNavigation(userId, nickname, mbti)
+        composable(NavRoutes.HOME) {
+            BottomNavigation()
         }
     }
 }
