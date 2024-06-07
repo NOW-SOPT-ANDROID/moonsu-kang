6b94cf829f74890a5a2e7495dd343f3cb0c5bedd
Moonsu Kang <rkdms4971@naver.com>
2024-06-07 23:52:10 +0900

    [REFACTOR/#17] suspend 함수로 변경

--- app/src/main/java/com/sopt/now/compose/AuthService.kt
+++ app/src/main/java/com/sopt/now/compose/AuthService.kt
@@ -5,23 +5,19 @@ import com.sopt.now.data.model.RequestSignUpDto
 import com.sopt.now.data.model.ResponseLoginDto
 import com.sopt.now.data.model.ResponseSignUpDto
 import com.sopt.now.data.model.ResponseUserInfoDto
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("member/join")
-    fun signUp(
-        @Body request: RequestSignUpDto
-    ): Call<ResponseSignUpDto>
+    suspend fun signUp(@Body signUpDto: RequestSignUpDto): Response<ResponseSignUpDto>
 
     @POST("/member/login")
-    fun login(
-        @Body request: RequestLoginDto
-    ): Call<ResponseLoginDto>
+    suspend fun login(@Body loginDto: RequestLoginDto): Response<ResponseLoginDto>
+
 
     @GET("/member/info")
-    fun getUserInfo(@Header("memberId") memberId: String): Call<ResponseUserInfoDto>
+    suspend fun getUserInfo(): Response<ResponseUserInfoDto>
 }
